29e4b8c41cd213ca295d111b7d8c9ecfbec74850
james smith <noletubby77@hotmail.com>
2018-08-19 09:55:42 -0400

    basic fields and formatting created, no calculations

---
+++ src/components/CheckoutTotal.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Grid, Divider, Input, Button } from "semantic-ui-react";
+
+export default class CheckoutTotal extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         promoCode: ""
+      }
+    }
+    
+  render() {
+    return (
+      <div>
+        <Grid divided="vertically">
+          <Grid.Column floated="left" width={8} >
+          <h4> ENTER PROMOTIONAL CODE OR GIFT CARD</h4>
+          </Grid.Column>
+          <Grid.Column floated="right" width={8} textAlign="right">
+          <Input action="APPLY" placeholder="CODE OR CARD" />
+          </Grid.Column>
+
+          <Grid.Column floated="left" width={8} >
+          <h4> SUB TOTAL</h4>
+          </Grid.Column>
+          <Grid.Column floated="right" width={8} textAlign="right">
+          </Grid.Column>
+
+          <Grid.Column floated="left" width={8} >
+          <h4> PROMOTION CODE {this.state.promoCode} APPLIED</h4>
+          </Grid.Column>
+          <Grid.Column floated="right" width={8} textAlign="right">
+          </Grid.Column>
+
+          <Grid.Column floated="left" width={8} >
+          <h4> ESTIMATED SHIPPING*</h4>
+          <p>You qualify for free shipping because your order is over $50</p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={8} textAlign="right">
+          </Grid.Column>
+
+          <Grid.Column floated="left" width={8} >
+          <h3> ESTIMATED TOTAL</h3>
+          <p>Tax will be applied during checkout</p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={8} textAlign="right">
+          </Grid.Column>
+          
+          <Grid.Column  width={10} >
+          </Grid.Column>
+          <Grid.Column  width={3} textAlign="right">
+          <h4><a href="#">CONTINUE SHOPPING</a></h4>
+          </Grid.Column>
+          <Grid.Column  width={3} textAlign="right">
+          <Button primary>CHECKOUT</Button>
+          </Grid.Column>
+
+        </Grid>
+      </div>
+    );
+  }
+}
