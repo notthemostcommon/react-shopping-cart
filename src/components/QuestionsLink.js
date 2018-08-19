5b73be263ef8a1e1e1139583905008595c4e4bcd
james smith <noletubby77@hotmail.com>
2018-08-19 09:43:47 -0400

    Question links added

---
+++ src/components/QuestionsLink.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h5>Need help or have questions?</h5>
+      <p>
+        Call Customer Service at <a href="tel:8005555555">1-800-555-5555</a>
+      </p>
+      <h5>
+        <a href="#">Chat with one of our systlist</a>
+      </h5>
+
+      <h5>
+        <a href="#">See return or exchange policy</a>
+      </h5>
+    </div>
+  );
+};
--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -1,46 +1,81 @@
-import React, { Component } from 'react'; 
-import { inventory } from '../inventory'; 
-import ShoppingList from '../components/ShoppingList';
+import React, { Component } from "react";
+import { inventory } from "../inventory";
+import ShoppingList from "../components/ShoppingList";
+import { Grid, Divider } from "semantic-ui-react";
+import CheckoutTotal from "../components/CheckoutTotal";
+import QuestionsLink from "../components/QuestionsLink";
 
 export default class Cart extends Component {
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props);
 
-  this.state = {
-     items: inventory
+    this.state = {
+      items: inventory,
+      listLength: 4
+    };
   }
-}
 
-renderList = items => {
+  renderList = items => {
     return items.map(item => {
-        console.log(item.src);
-        
-        // const imgUrl = require(`${item.src}`);  
-        // console.log(imgUrl);
-        
-        return (
-            <ShoppingList
-                key={item.styleNumber}
-                image={item.src}
-                name={item.name} 
-                styleNumber={item.styleNumber} 
-                color={item.color}
-                alt={item.alt} 
-                size={item.size}
-                price={item.price}
-                render={item.render} 
-                />
+      console.log(this.state.listLength);
+
+      return (
+        item.render && (
+          <ShoppingList
+            key={item.styleNumber}
+            image={item.src}
+            name={item.name}
+            styleNumber={item.styleNumber}
+            color={item.color}
+            alt={item.alt}
+            size={item.size}
+            price={item.price}
+            render={item.render}
+          />
         )
-    })
-}
+      );
+    });
+  };
 
   render() {
-      
     return (
       <div>
-        <h1>Cart.js</h1>
+        <h1>YOUR SHOPPING CART</h1>
+        <p>
+          If the cart if completely empty then we shall again add back the
+          products for you
+        </p>
+        <Grid divided="vertically">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <h4> {this.state.listLength} ITEMS </h4>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <h4>DESCRIPTION</h4>
+            </Grid.Column>
+            <Grid.Column width={2} textAlign="center">
+              <h4>SIZE</h4>
+            </Grid.Column>
+            <Grid.Column width={2} textAlign="center">
+              <h4>QTY</h4>
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              <h4>PRICE</h4>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider />
         {this.renderList(this.state.items)}
+        <Divider />
+        <Grid>
+          <Grid.Column floated="left" width={4} >
+          <QuestionsLink/>
+          </Grid.Column>
+          <Grid.Column floated="right" width={12}>
+            <CheckoutTotal />
+          </Grid.Column>
+        </Grid>
       </div>
-    )
+    );
   }
 }
