79cdc2843916e4f5cae51a6c70cac6e4a859f3c2
james smith <noletubby77@hotmail.com>
2018-08-20 09:26:57 -0400

    edit modal formatted, pricing and sizing updates

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,25 +1,74 @@
 import React from "react";
-import { Modal, Button, Image, Header } from "semantic-ui-react";
+import {
+  Modal,
+  Button,
+  Image,
+  Grid,
+  Dropdown
+} from "semantic-ui-react";
 
-const EditItem = (props) => {
-    // console.log(props);
-    
+const EditItem = props => {
+  console.log(props);
+  const sizingStyle = { display: "inline-block" };
+  const inputStyle = { height: "40px", textAlign: "center" };
+  const options = [
+    { key: 1, text: "Small", value: "Small" },
+    { key: 2, text: "Medium", value: "Medium" },
+    { key: 3, text: "Large", value: "Large" },
+    { key: 4, text: "X-Large", value: "X-Large" }
+  ];
   return (
-    <Modal trigger={<h4>EDIT</h4>}>
-      <Modal.Header>Select a Photo</Modal.Header>
-      <Modal.Content image>
-        <Image wrapped size="medium" src="/images/avatar/large/rachel.png" />
-        <Modal.Description>
-          <Header>Default Profile Image</Header>
-          <p>
-            We've found the following gravatar image associated with your e-mail
-            address.
-          </p>
-          <p>Is it okay to use this photo?</p>
-        </Modal.Description>
-      </Modal.Content>
+    <Modal trigger={<h5>EDIT</h5>} closeIcon>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column textAlign="center" width={8}>
+            <h3>{`${props.item.name}`.toUpperCase()}</h3>
+            <h2>${props.price}</h2>
+            <br />
+            <h4>{props.item.styleNumber}</h4>
+            {props.item.colorOptions.map((color, i) => {
+              const swatchStyle = {
+                background: `${color}`,
+                borderColor: "black",
+                borderWidth: "5px",
+                width: "35px",
+                height: "25px",
+                display: "inline-block"
+              };
+              return <div style={swatchStyle} key={i} />;
+            })}
+            <h4>Color: {props.item.color}</h4>
+
+            <div style={sizingStyle}>
+              <Dropdown
+                selection
+                options={options}
+                // placeholder={props.shirtSize}
+                onChange={props.updateShirtSize}
+                value={props.shirtSize}
+              />
+              <input
+                maxLength="1"
+                defaultValue={props.quantity}
+                size="2"
+                align="middle"
+                
+                style={inputStyle}
+                onBlur={props.updateQuantity}
+              />
+            </div>
+            <br/>
+            <Button primary>EDIT</Button>
+            <p><a href="#">Check product details</a></p>
+
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Image src={props.item.image} size="medium" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Modal>
   );
 };
 
-export default EditItem; 
+export default EditItem;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,50 +3,100 @@ import { Grid, Image, Divider } from "semantic-ui-react";
 import EditItem from "./EditItem";
 
 export default class ShoppingList extends Component {
-   constructor(props) {
-     super(props)
-   
-     this.state = {
-     }
-   }
-   
-   
-    
-    render(){
-        const upperName = `${this.props.name}`.toUpperCase();
-        const shirtSize = this.props.size[Math.floor(Math.random() * this.props.size.length)];
-        const price = this.props.price.toFixed(2);
-      //   const totalItems = props.length;
-      return (
-        <div>
-          <Grid divided="vertically">
-            <Grid.Row>
-              <Grid.Column width={3}>
-                <Image src={this.props.image} size="small" />
-              </Grid.Column>
-              <Grid.Column width={6}>
-                <h3>{upperName}</h3>
-                <p>Style #: {this.props.styleNumber}</p>
-                <p>Color: {this.props.color}</p>
-                <br/>
-                <EditItem /> <h4>| X REMOVE | SAVE FOR LATER </h4>
-                
-              </Grid.Column>
-              <Grid.Column width={2} textAlign="center">
-                <h3>{shirtSize}</h3>
-              </Grid.Column>
-              <Grid.Column width={2} textAlign="center">
-                <input maxLength="1" defaultValue="1" size="2" align="middle" />
-              </Grid.Column>
-    
-              <Grid.Column width={3} textAlign="right">
-                <h2>${price}</h2>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <Divider />
-        </div>
-      );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shirtSize: "",
+      quantity: 1,
+      price: ""
     };
-}; 
-  
+    this.updateShirtSize = this.updateShirtSize.bind(this);
+    this.updateQuantity = this.updateQuantity.bind(this);
+    this.updatePrice = this.updatePrice.bind(this); 
+  }
+
+  componentDidMount = () => {
+    const shirtSize = this.props.size[
+      Math.floor(Math.random() * this.props.size.length)
+    ];
+    const price = (this.props.price * parseInt(this.state.quantity)).toFixed(2);
+
+    this.setState({ shirtSize: shirtSize, price: price });
+  };
+
+  updateShirtSize = (e, { value }) => {
+    this.setState({ shirtSize: value });
+  };
+
+  updateQuantity = e => {
+      console.log("inside updateQuantity");
+      
+    this.setState({ quantity: e.target.value });
+    this.updatePrice(e.target.value);
+    console.log(this.state.quantity);
+    
+  };
+
+  updatePrice = (quant) => {
+      console.log("inside updatePrice");
+      
+    const price = (this.state.price * quant).toFixed(2);
+    this.props.updateTotal(price); 
+    this.setState({ price: price });
+  };
+
+  render() {
+    console.log("shoppinglist state", this.state);
+    const editButtonStyle = {display: "flex", flexDirection: "row"}; 
+    const price = (this.props.price.toFixed(2));
+
+    const upperName = `${this.props.name}`.toUpperCase();
+    return (
+      <div>
+        <Grid divided="vertically">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image src={this.props.image} size="small" />
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <h3>{upperName}</h3>
+              <p>Style #: {this.props.styleNumber}</p>
+              <p>Color: {this.props.color}</p>
+              <br />
+              <Grid.Row>
+
+              <h5 style={editButtonStyle}>
+              <EditItem
+                item={this.props}
+                price={price}
+                shirtSize={this.state.shirtSize}
+                updateShirtSize={this.updateShirtSize}
+                quantity={this.state.quantity}
+                updateQuantity={this.updateQuantity}
+                />
+              | X REMOVE | SAVE FOR LATER </h5>
+                </Grid.Row>
+            </Grid.Column>
+            <Grid.Column width={2} textAlign="center">
+              <h3>{this.state.shirtSize}</h3>
+            </Grid.Column>
+            <Grid.Column width={2} textAlign="center">
+              <input
+                maxLength="1"
+                value={this.state.quantity}
+                size="2"
+                align="middle"
+              />
+            </Grid.Column>
+
+            <Grid.Column width={3} textAlign="right">
+              <h2>${this.state.price}</h2>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider />
+      </div>
+    );
+  }
+}
