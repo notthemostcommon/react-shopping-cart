6a90fd6436c18016c2c368d2c491a5d15bfff81e
james smith <noletubby77@hotmail.com>
2018-08-20 04:35:59 -0400

    list of chosen items renders

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { inventory } from "../inventory";
 import ShoppingList from "../components/ShoppingList";
-import { Grid, Divider } from "semantic-ui-react";
+import { Grid, Divider, Container } from "semantic-ui-react";
 import CheckoutTotal from "../components/CheckoutTotal";
 import QuestionsLink from "../components/QuestionsLink";
+
 
 export default class Cart extends Component {
   constructor(props) {
@@ -11,14 +12,33 @@ export default class Cart extends Component {
 
     this.state = {
       items: inventory,
-      listLength: 4
+      listLength: 0,
+      chosenItems: [],
+      totalAmount: []
     };
+
+    this.updateTotal = this.updateTotal.bind(this);
   }
+
+  componentDidMount = () => {
+    let renderedList = [];
+    this.state.items.map(item => {
+      item.render && renderedList.push(item);
+    });
+    this.setState({
+      chosenItems: renderedList,
+      listLength: renderedList.length
+    });
+  };
+  updateTotal = () => {
+    let { totalAmount } = this.state;
+    this.state.chosenItems.map(item => {
+      return totalAmount.push(item.price);
+    });
+  };
 
   renderList = items => {
     return items.map(item => {
-      console.log(this.state.listLength);
-
       return (
         item.render && (
           <ShoppingList
@@ -38,43 +58,50 @@ export default class Cart extends Component {
   };
 
   render() {
+    this.updateTotal();
+    console.log("this state in render", this.state);
+
     return (
       <div>
-        <h1>YOUR SHOPPING CART</h1>
-        <p>
-          If the cart if completely empty then we shall again add back the
-          products for you
-        </p>
-        <Grid divided="vertically">
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <h4> {this.state.listLength} ITEMS </h4>
+        <Container>
+          <h1>YOUR SHOPPING CART</h1>
+          <p>
+            If the cart if completely empty then we shall again add back the
+            products for you
+          </p>
+
+          <Grid divided="vertically">
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <h4> {this.state.listLength} ITEMS </h4>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <h4>DESCRIPTION</h4>
+              </Grid.Column>
+              <Grid.Column width={2} textAlign="center">
+                <h4>SIZE</h4>
+              </Grid.Column>
+              <Grid.Column width={2} textAlign="center">
+                <h4>QTY</h4>
+              </Grid.Column>
+              <Grid.Column width={3} textAlign="right">
+                <h4>PRICE</h4>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Divider />
+          
+          {this.renderList(this.state.items)}
+]          <Divider />
+          <Grid>
+            <Grid.Column floated="left" width={4}>
+              <QuestionsLink />
             </Grid.Column>
-            <Grid.Column width={6}>
-              <h4>DESCRIPTION</h4>
+            <Grid.Column floated="right" width={12}>
+              <CheckoutTotal />
             </Grid.Column>
-            <Grid.Column width={2} textAlign="center">
-              <h4>SIZE</h4>
-            </Grid.Column>
-            <Grid.Column width={2} textAlign="center">
-              <h4>QTY</h4>
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              <h4>PRICE</h4>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <Divider />
-        {this.renderList(this.state.items)}
-        <Divider />
-        <Grid>
-          <Grid.Column floated="left" width={4} >
-          <QuestionsLink/>
-          </Grid.Column>
-          <Grid.Column floated="right" width={12}>
-            <CheckoutTotal />
-          </Grid.Column>
-        </Grid>
+          </Grid>
+        </Container>
       </div>
     );
   }
